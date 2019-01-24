99767f62e96df52286d9256acc47a138d17e15e9
Jimmy Lv <jimmy.jinglv@gmail.com>
2019-01-24 16:12:21 +0800

    feat(example): added shoppingCart, product detail and list

---
+++ src/views/cart/shoppingCart.vue
@@ -0,0 +1,31 @@
+<template>
+  <cafe-responsive>
+    <cp-cartDetail v-show="!isEmpty" @empty="emptyFn" />
+    <div v-show="isEmpty">empty cart</div>
+  </cafe-responsive>
+</template>
+<script>
+import cpCartDetail from '@/views/cart/cp-cartDetail.vue'
+
+export default {
+  name: 'ShoppingCart',
+  title() {
+    return 'Kidz 购物车'
+  },
+  components: {
+    cpCartDetail,
+  },
+  data() {
+    return {
+      isEmpty: false, // 如果购物车是空的 就展示 空的组件
+    }
+  },
+  computed: {},
+  mounted() {},
+  methods: {
+    emptyFn(bool) {
+      this.isEmpty = bool
+    },
+  },
+}
+</script>
--- src/views/product/detail.js
+++ src/views/product/detail.js
@@ -1,9 +1,21 @@
 import ProductDetail from '@/views/product/detail.vue'
 
+import { gqlClient } from '@/plugins/gqlClient'
+import PRODUCT_DETAIL from '@/api/schema/productDetail.gql'
+
 export default {
   name: 'ProductDetail',
   data() {
     return {}
+  },
+  async mounted() {
+    const res = await gqlClient.query({
+      query: PRODUCT_DETAIL,
+      variables: {
+        codes: ['144'],
+      },
+    })
+    console.log('res example............', res.data)
   },
   render() {
     return <ProductDetail />
